dd10f0648c5a7c8542f86af15917356b9120d28a
dixdix <reg.dev@am35.fr>
2022-10-24 12:06:53 +0200

    feat(userscripts): DataGalaxy ID Extractor

---
+++ userscripts/DataGalaxyIDs.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         DataGalaxy ID Extractor
+// @namespace    http://github.com/dix
+// @version      2022.10.24.0
+// @description  KISS Script to retrieve ID of DataGalaxy object from the UI
+// @author       http://github.com/dix
+// @match        https://*.datagalaxy.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=datagalaxy.com
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    document.onclick = function () {
+        const props = document.getElementsByClassName('ag-row');
+        if (props && props.length > 0) {
+            Array.from(props).forEach((el) => {
+                el.onclick = function (e) {
+                    if (e.shiftKey) {
+                        prompt(`ID du Usage [${el.getElementsByClassName('name')[0].innerHTML}]`, el.getAttribute('row-id'));
+                    }
+
+                };
+            });
+        }
+    };
+})();
