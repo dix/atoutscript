c229277b0a09c0cc05c8c665acb1ab3f2d34f9d8
dixdix <reg.dev@am35.fr>
2022-12-01 16:38:57 +0100

    fix(datagalaxy): New version

--- userscripts/DataGalaxyIDs.user.js
+++ userscripts/DataGalaxyIDs.user.js
@@ -1,22 +1,22 @@
 // ==UserScript==
 // @name         DataGalaxy ID Extractor
 // @namespace    http://github.com/dix
-// @version      2022.10.24.0
+// @version      2022.12.01.0
 // @description  KISS Script to retrieve ID of DataGalaxy object from the UI
 // @author       http://github.com/dix
 // @match        https://*.datagalaxy.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=datagalaxy.com
 // ==/UserScript==
 
-(function () {
+(function() {
     'use strict';
-    document.onclick = function () {
+    document.onclick = function(){
         const props = document.getElementsByClassName('ag-row');
-        if (props && props.length > 0) {
+        if(props && props.length > 0){
             Array.from(props).forEach((el) => {
-                el.onclick = function (e) {
-                    if (e.shiftKey) {
-                        prompt(`ID du Usage [${el.getElementsByClassName('name')[0].innerHTML}]`, el.getAttribute('row-id'));
+                el.onclick = function(e){
+                    if(e.shiftKey){
+                        prompt(`ID du Usage [${el.getElementsByClassName('entity-name')[0].innerHTML}]`, el.getAttribute('row-id'));
                     }
 
                 };
